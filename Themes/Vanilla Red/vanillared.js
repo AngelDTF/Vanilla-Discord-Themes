2e11d1e65b060b234e511b744b0622ec83a4e28f
TheonTutorials <43016525+TheonTutorials@users.noreply.github.com>
2020-02-14 15:49:48 -0500

    Added 'Vanilla Red'
    Added a new Vanilla Discord Theme titled "Vanilla Red".

---
+++ Themes/Vanilla Red/vanillared.js
@@ -0,0 +1,6 @@
+document.body.style.setProperty('--background-primary', '#680019');
+document.body.style.setProperty('--background-secondary', '#530013');
+document.body.style.setProperty('--background-secondary-alt', '#4B0E20');
+document.body.style.setProperty('--background-tertiary', '#260008');
+document.body.style.setProperty('--background-floating', '#200008');
+document.body.style.setProperty('--channeltextarea-background', '#5E2A3A');
