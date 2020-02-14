68ee2cc553f65ab3e224c8e8f01ef0831543f118
TheonTutorials <43016525+TheonTutorials@users.noreply.github.com>
2020-02-14 14:28:31 -0500

    Create vanillablue.js

---
+++ Themes/Vanilla Blue/vanillablue.js
@@ -0,0 +1,6 @@
+document.body.style.setProperty('--background-primary', '#002451');
+document.body.style.setProperty('--background-secondary', '#001C40');
+document.body.style.setProperty('--background-secondary-alt', '#15273F');
+document.body.style.setProperty('--background-tertiary', '#000D1E');
+document.body.style.setProperty('--background-floating', '#000B19');
+document.body.style.setProperty('--channeltextarea-background', '#304054');
