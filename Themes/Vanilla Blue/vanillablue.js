359aa5f4c1fa9fb09418630b615430c0d4c08cc5
TheonTutorials <43016525+TheonTutorials@users.noreply.github.com>
2020-02-15 15:41:42 -0500

    Updated Vanilla Blue

--- Themes/Vanilla Blue/vanillablue.js
+++ Themes/Vanilla Blue/vanillablue.js
@@ -4,3 +4,6 @@ document.body.style.setProperty('--background-secondary-alt', '#15273F');
 document.body.style.setProperty('--background-tertiary', '#000D1E');
 document.body.style.setProperty('--background-floating', '#000B19');
 document.body.style.setProperty('--channeltextarea-background', '#304054');
+var styleselem = document.createElement('style');
+document.head.appendChild(styleselem);
+styleselem.append('.theme-dark .scrollerThemed-2oenus.themedWithTrack-q8E3vB .scroller-2FKFPG::-webkit-scrollbar-thumb { background-color: #304054; } .theme-dark .scrollerThemed-2oenus.themedWithTrack-q8E3vB .scroller-2FKFPG::-webkit-scrollbar-track-piece { background-color: #15273F; border-color: #002451; } .peopleColumn-29fq28 { background-color: #002451; } .theme-dark .root-1gCeng { background-color: #002451; } .theme-dark .footer-3rDWdC { background-color: #15273F; } .theme-dark .modal-yWgWj- { background-color: #002451; } .theme-dark .footer-3rDWdC { background-color: #15273F; } .theme-dark .bodyInner-245q0L, .theme-dark .footer-1fjuF6 { background-color: #15273F; } .theme-dark .quickMessage-1yeL4E { background-color: #20324a; } .theme-dark .uploadModal-2ifh8j { background-color: #002451; } .theme-dark .footer-3mqk7D { background-color: #15273F; }');
