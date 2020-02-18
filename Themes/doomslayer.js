4cab06d307acb36be55d34190e7fc8f1f61861e1
TheonTutorials <43016525+TheonTutorials@users.noreply.github.com>
2020-02-18 15:23:17 -0500

    Added Doom Slayer theme

---
+++ Themes/doomslayer.js
@@ -0,0 +1,4 @@
+var styleselem = document.createElement('style');
+document.head.appendChild(styleselem);
+styleselem.append('body { background-image: url(https://cdn.discordapp.com/attachments/679413891054436363/679413934993702932/bg.png); background-size: 100% 100%; background-repeat: no-repeat; } .appMount-3lHmkl { opacity: 0.75; }');
+console.log('[Vanilla Discord Themes] Doom Slayer applied.');
